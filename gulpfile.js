e3c2c87894f4a786db2b5c04fef3abb98353cefb
miya0001 <miya@wpist.me>
2016-01-21 06:36:01 +0900

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var download = require( "gulp-download" );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' );
+
+gulp.task( 'download', function () {
+	return download( [
+			'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/controls/VRControls.js',
+			'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/effects/VREffect.js',
+			'https://raw.githubusercontent.com/borismus/webvr-boilerplate/master/bower_components/webvr-polyfill/build/webvr-polyfill.js',
+			'https://raw.githubusercontent.com/borismus/webvr-boilerplate/master/build/webvr-manager.js'
+		] )
+		.pipe( concat( 'three-plugins.min.js' ) )
+		.pipe( uglify() )
+		.pipe( gulp.dest( 'js' ) );
+} );
+
+gulp.task( 'default', [ 'download' ], function () {
+	return gulp.src( [
+			'node_modules/three/three.min.js',
+		] )
+		.pipe( gulp.dest( 'js' ) );
+} );
