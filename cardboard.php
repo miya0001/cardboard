b3e00ae446ec1b8538ec20fdb77756a57f4c83ee
miya0001 <miya@wpist.me>
2016-01-23 00:53:33 +0900

    some fix

--- cardboard.php
+++ cardboard.php
@@ -20,24 +20,46 @@ add_action( "add_attachment", function( $post_id ){
 	}
 } );
 
-add_filter( 'image_send_to_editor', function( $html, $post_id ) {
-	if ( get_post_meta( $post_id, 'is_panorama_photo' ) ) {
+add_filter( 'image_send_to_editor', function( $html, $post_id, $caption, $title, $align, $url, $size, $alt ) {
+	if ( get_post_meta( $post_id, 'is_panorama_photo' ) && ( ! is_array( $size ) && 'full' === $size ) ) {
 		return '[cardboard id="' . esc_attr( $post_id ) . '"]';
 	} else {
 		return $html;
 	}
-}, 10, 2 );
+}, 10, 8 );
 
 add_shortcode( 'cardboard', function( $p, $content ) {
 	if ( intval( $p['id'] ) ) {
 		$src = wp_get_attachment_image_src( $p['id'], 'full' );
 		if ( $src ) {
 			return sprintf(
-				'<div class="cardboard" data-image="%s"></div>',
+				'<div class="cardboard" data-image="%s"><a class="full-screen"><span class="dashicons dashicons-editor-expand"></span></a></div>',
 				esc_url( $src[0] )
 			);
 		}
 	}
+} );
+
+add_action( 'wp_head', function() {
+	?>
+	<style>
+	.cardboard
+	{
+		position: relative;
+	}
+	.cardboard .full-screen
+	{
+		display: block;
+		position: absolute;
+		bottom: 8px;
+		right: 8px;
+		z-index: 999;
+		color: #ffffff;
+		text-decoration: none;
+		border: none;
+	}
+	</style>
+	<?php
 } );
 
 add_action( "wp_enqueue_scripts", function() {
