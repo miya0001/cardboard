97094d3819d3555a14dfb207ad8944a36747265e
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-12-16 15:34:32 +0900

    add endpoint test.

--- tests/test-cardboard.php
+++ tests/test-cardboard.php
@@ -19,4 +19,23 @@ class Cardboard_Test extends WP_UnitTestCase
 		$result = Cardboard::is_panorama_photo( dirname( __FILE__ ) . '/img/not-jpeg.png' );
 		$this->assertFalse( $result );
 	}
+
+	/**
+	 * @test
+	 */
+	function is_able_to_access_cardbord() {
+		$attachment = $this->factory->attachment->create();
+		$this->go_to( home_url( 'cardboard/'. $attachment ) );
+		$this->assertFalse( is_404() );
+	}
+
+	/**
+	 * @test
+	 */
+	function test_rewrite_endpoint() {
+		global $wp_rewrite;
+		$this->assertContains( array( EP_ROOT, Cardboard::QUERY_VAR, Cardboard::QUERY_VAR ) , $wp_rewrite->endpoints );
+		Cardboard::remove_rewrite_endpoint();
+		$this->assertNotContains( array( EP_ROOT, Cardboard::QUERY_VAR, Cardboard::QUERY_VAR ) , $wp_rewrite->endpoints );
+	}
 }
